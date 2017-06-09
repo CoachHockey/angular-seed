0bc920f9d827c359a33e989cdbd44cc6404b99a1
Raven Demeyer <raven.demeyer@gmail.com>
2017-06-09 17:35:01 +0200

    clarify factory exports

--- src/client/app/aot/test-api.cache.factory.ts
+++ src/client/app/aot/test-api.cache.factory.ts
@@ -2,6 +2,8 @@
 import { IndexedDBCache } from '../indexeddb-cache/indexeddb-cache';
 import { IndexedDBStore } from '../indexeddb-cache/indexeddb-store';
 
+// factory functions for FactoryProviders as exports instead of inline:
+// https://github.com/rangle/angular-2-aot-sandbox#func-in-providers-usefactory-top
 export function mainIndexedDBCacheFactory() { return new IndexedDBCache('main'); }
 export function userIndexedDBStoreFactory(testIndexedDBCache: IndexedDBCache) { return new IndexedDBStore(testIndexedDBCache, 'user'); }
 export function taskIndexedDBStoreFactory(testIndexedDBCache: IndexedDBCache) { return new IndexedDBStore(testIndexedDBCache, 'task'); }
