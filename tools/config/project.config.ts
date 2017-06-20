47bbf7514c8d590862a8c36f8ec36195a47cab39
Raven Demeyer <raven.demeyer@gmail.com>
2017-06-20 13:21:08 +0200

    Implement SASS

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -58,6 +58,11 @@ export class ProjectConfig extends SeedConfig {
 
     /* Add to or override NPM module configurations: */
     // this.PLUGIN_CONFIGS['browser-sync'] = { ghostMode: false };
+
+    /* Enable SASS / SCSS */
+    // https://github.com/mgechev/angular-seed/wiki/Working-with-SASS
+    this.ENABLE_SCSS = true;
+    //this.SCSS_SRC = `${this.APP_SRC}/scss`
   }
 
 }
